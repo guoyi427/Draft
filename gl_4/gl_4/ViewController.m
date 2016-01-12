830e2339d63f1e22d1fe538f9698830c7446c09d
Yi Guo <yi.guo@10020.net>
2016-01-12 11:10:46 +0800

    opengl

---
+++ gl_4/gl_4/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  gl_4
+//
+//  Created by guoyi on 16/1/12.
+//  Copyright © 2016年 郭毅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ gl_4/gl_4Tests/gl_4Tests.m
@@ -0,0 +1,39 @@
+//
+//  gl_4Tests.m
+//  gl_4Tests
+//
+//  Created by guoyi on 16/1/12.
+//  Copyright © 2016年 郭毅. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface gl_4Tests : XCTestCase
+
+@end
+
+@implementation gl_4Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
