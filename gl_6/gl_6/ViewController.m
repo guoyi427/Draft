16fcdbbaa4e592d2edc282b01c88588a995d1a91
Yi Guo <yi.guo@10020.net>
2016-01-14 21:06:56 +0800

    空白

---
+++ gl_6/gl_6/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  gl_6
+//
+//  Created by guoyi on 16/1/13.
+//  Copyright © 2016年 郭毅. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
